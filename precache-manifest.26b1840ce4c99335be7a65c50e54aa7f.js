41773d411596dac715d5ca7a060fb8bd738be3cc
Fernando Martinez Mojica <fernando.mojica@utexas.edu>
2020-04-20 19:44:57 -0500

    Updates

---
+++ precache-manifest.26b1840ce4c99335be7a65c50e54aa7f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341e003c604764534a3399583cc028aa",
+    "url": "/tech-report-demo/index.html"
+  },
+  {
+    "revision": "c917df800a6d68e07413",
+    "url": "/tech-report-demo/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "40019fb8a113bf202959",
+    "url": "/tech-report-demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c917df800a6d68e07413",
+    "url": "/tech-report-demo/static/js/2.b25ef33a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tech-report-demo/static/js/2.b25ef33a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40019fb8a113bf202959",
+    "url": "/tech-report-demo/static/js/main.d760fdae.chunk.js"
+  },
+  {
+    "revision": "5f8de33b3a8a41ef42d6",
+    "url": "/tech-report-demo/static/js/runtime-main.9860b279.js"
+  },
+  {
+    "revision": "b318fccd933884342a8658b8421f73a1",
+    "url": "/tech-report-demo/static/media/DSLR.b318fccd.jpg"
+  },
+  {
+    "revision": "24e87fba812f31dfe96c502be923f511",
+    "url": "/tech-report-demo/static/media/Shoes.24e87fba.jpg"
+  },
+  {
+    "revision": "512ce26896221a872ab67a7136709c3c",
+    "url": "/tech-report-demo/static/media/Wallet.512ce268.jpg"
+  }
+]);
